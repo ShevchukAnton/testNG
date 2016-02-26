d296b27b14e78b0bd17cd7326c15b09e889e3f0b
Anton <Ssniperok@gmail.com>
2016-02-26 17:30:55 +0200

    testNG

---
+++ src/main/java/FractionNumOperations.java
@@ -0,0 +1,49 @@
+/**
+ * Created by anton on 19.10.15.
+ */
+public class FractionNumOperations {
+
+    FractionNumOperations() {
+
+    }
+
+    public FractionNumber add(FractionNumber a, FractionNumber b) throws IllegalArgumentException {
+        if (a == null || b == null) {
+            throw new IllegalArgumentException("Arguments Can't be null");
+        }
+        int dividend = (a.getDividend() * b.getDivisor()) + (b.getDividend() * a.getDivisor());
+        int divisor = (a.getDivisor() * b.getDivisor());
+        return new FractionNumber(dividend, divisor);
+    }
+
+    public FractionNumber div(FractionNumber a, FractionNumber b) throws IllegalArgumentException {
+        if (a == null || b == null) {
+            throw new IllegalArgumentException("Arguments Can't be null");
+        }
+        int dividend = (a.getDividend() * b.getDivisor()) - (b.getDividend() * a.getDivisor());
+        int divisor = (a.getDivisor() * b.getDivisor());
+
+        return new FractionNumber(dividend, divisor);
+    }
+
+    public FractionNumber mul(FractionNumber a, FractionNumber b) throws IllegalArgumentException {
+        if (a == null || b == null) {
+            throw new IllegalArgumentException("Arguments Can't be null");
+        }
+        int dividend = a.getDividend() * b.getDividend();
+        int divisor = a.getDivisor() * b.getDivisor();
+        return new FractionNumber(dividend, divisor);
+    }
+
+    public FractionNumber sub(FractionNumber a, FractionNumber b) throws IllegalArgumentException, ArithmeticException {
+        if (a == null || b == null) {
+            throw new IllegalArgumentException("Arguments Can't be null");
+        }
+        int divisor = a.getDivisor() * b.getDividend();
+        if (divisor == 0) {
+            throw new ArithmeticException("Divisor can't be 0");
+        }
+        int dividend = a.getDividend() * b.getDivisor();
+        return new FractionNumber(dividend, divisor);
+    }
+}
---
+++ src/main/java/FractionNumber.java
@@ -0,0 +1,49 @@
+/**
+ * Created by anton on 19.10.15.
+ */
+public class FractionNumber {
+
+    private int dividend;
+    private int divisor;
+
+    FractionNumber(int dividend, int divisor) {
+        this.dividend = dividend;
+        this.divisor = divisor;
+    }
+
+    FractionNumber() {
+        dividend = 1;
+        divisor = 1;
+    }
+
+    public void setDividend(int dividend) {
+        this.dividend = dividend;
+    }
+
+    public int getDividend() {
+        return dividend;
+    }
+
+    public void setDivisor(int divisor) throws IllegalArgumentException {
+        if (divisor == 0) {
+            throw new IllegalArgumentException("Divisor can't be 0");
+        }
+        this.divisor = divisor;
+    }
+
+    public int getDivisor() {
+        return divisor;
+    }
+
+    public double value() {
+        double result = (double) dividend / divisor;
+        return Math.rint(result * 100) / 100;
+    }
+
+    public String toStringValue() {
+        if (divisor == 1) {
+            return dividend + "\n";
+        }
+        return dividend + "/" + divisor;
+    }
+}
